596de4adc81aed0a3577099a8d2ab79d88dec6cc
Leonid Petukhov <rusadrako@yandex.ru>
2025-02-03 06:23:56 +0300

    Обработчики проверки

---
+++ tests/handler/data__control__class.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace tests\data_set;
+
+
+
+/**
+ * @author Петухов Леонид <l.petuhov@okonti.ru>
+ */
+class check__class {
+
+	/**  */
+	public static function check_account() {
+		return [
+			'GUID'                => null,
+		];
+	}
+
+
+
+	/**  */
+	public static function check_transaction() {
+		return [
+			'GUID'                => null,
+		];
+	}
+
+
+
+/**/
+}
